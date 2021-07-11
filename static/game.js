856df3a807e2570a78a142fc417e67146cbb2209
geekvert <rahuliitr258@gmail.com>
2021-07-11 11:44:47 +0530

    Code refactoring

--- static/game.js
+++ static/game.js
@@ -1,11 +1,11 @@
-document.querySelector('body').onload=function() {
-    var i=0;
-    var txt='Minesweeper';
-    var speed=90;
+document.querySelector('body').onload = function() {
+    var i = 0;
+    var txt = 'Minesweeper';
+    var speed = 100;
 
     function typeWriter() {
-        if (i<txt.length) {
-            document.querySelector('.textBox').innerHTML+=txt.charAt(i);
+        if (i < txt.length) {
+            document.querySelector('.textBox').innerHTML += txt.charAt(i);
             i++;
             setTimeout(typeWriter, speed);
         }
@@ -13,51 +13,53 @@ document.querySelector('body').onload=function() {
     typeWriter();
 }
 
-var i=0;
+var i = 0;
 var timeout;
-var timer=document.querySelector('.timer');
-var counting=function() {
+var timer = document.querySelector('.timer');
+var counting = function() {
     i++;
-    timer.innerHTML=i+'<span style="font-size: 60%;"> seconds</span>';
-    timeout=setTimeout(counting, 1000);
+    timer.innerHTML = i+'<span style="font-size: 60%;"> seconds</span>';
+    timeout = setTimeout(counting, 1000);
 }
 
-var container=document.querySelector('.container');
+var container = document.querySelector('.container');
 
-var toArray=function(obj) {
-    var array=[];
+var toArray = function(obj) {
+    var array = [];
     for (var i=0; i<obj.length; i++) { 
-      array[i]=obj[i];
+      array[i] = obj[i];
     }
     return array;
 }
 
-var ranNums=function(lim, number) {
-    var ar=[number];
+var ranNums = function(lim, number) {
+    var ar = new Array(number);
 
     for (var i=0; i<number; i++) {
-        ar[i]=[number];
+        ar[i] = new Array(number);
     }
 
     for (var i=0; i<number; i++) {
         for (var j=0; j<number; j++) {
-            var obj={a:0,b:0};
-            obj.a=Math.ceil(Math.random()*lim);
-            obj.b=Math.ceil(Math.random()*lim);
-            ar[i][j]=obj;
+            var obj = {a:0, b:0};
+            obj.a = Math.ceil(Math.random()*lim);
+            obj.b = Math.ceil(Math.random()*lim);
+            ar[i][j] = obj;
         }
     }
     return ar;
 }
+
 var blocks;
 var createMaze=function(n) {
-    blocks=[n];
+    blocks = [n];
     for (var i=0; i<n; i++) {
-        blocks[i]=[n];
+        blocks[i] = new Array(n);
     }
 
-    var check=function(i,j) {
-        try {if (toArray(blocks[i][j].classList).includes('bomb'))
+    var checkForBomb = function(i, j) {
+        try {
+            if (toArray(blocks[i][j].classList).includes('bomb'))
             return true;
         }
         catch (e) {
@@ -66,11 +68,11 @@ var createMaze=function(n) {
     }
     
     //getting random number array;
-    var twoD=ranNums(n-1, n/2);
+    var twoD = ranNums(n-1, n/2);
 
     for (var i=0; i<n; i++) {
         for (var j=0; j<n; j++) {
-            blocks[i][j]=document.createElement('div');
+            blocks[i][j] = document.createElement('div');
             blocks[i][j].classList.add('block');
             //container.appendChild(blocks[i][j]);
         }
@@ -79,28 +81,28 @@ var createMaze=function(n) {
     //assigning BOMBs
     for (var i=0; i<n/2; i++) {
         for (var j=0; j<n/2; j++) {
-            blocks[twoD[i][j].a][twoD[i][j].b].classList.add('bomb');
+            blocks[twoD[i][j].a][twoD[i][j].b].classList.add('bomb');            
         }
     }
 
     //putting numbers in place
     for (var i=0; i<n; i++) {
         for (var j=0; j<n; j++) {
-            var count=0;
-            if (check(i,j)) {
+            var count = 0;
+            if (checkForBomb(i,j)) {
                 continue;
             }
             else {
-                if (check(i-1,j-1)) count++;
-                if (check(i-1,j)) count++;
-                if (check(i-1,j+1)) count++;
-                if (check(i,j-1)) count++;
-                if (check(i,j+1)) count++;
-                if (check(i+1,j-1)) count++;
-                if (check(i+1,j)) count++;
-                if (check(i+1,j+1)) count++;
+                if (checkForBomb(i-1,j-1)) count++;
+                if (checkForBomb(i-1,j)) count++;
+                if (checkForBomb(i-1,j+1)) count++;
+                if (checkForBomb(i,j-1)) count++;
+                if (checkForBomb(i,j+1)) count++;
+                if (checkForBomb(i+1,j-1)) count++;
+                if (checkForBomb(i+1,j)) count++;
+                if (checkForBomb(i+1,j+1)) count++;
                 if (count!=0) {
-                    blocks[i][j].textContent=count;
+                    blocks[i][j].textContent = count;
                 }
             }
         }
@@ -116,63 +118,62 @@ var createMaze=function(n) {
     }
 }
 
-var coverBlocks=function(n) {
-        for (var i=0; i<n; i++) {
-            for (var j=0; j<n; j++) {
-                var temp=document.createElement('div');
-                temp.classList.add('blockLayer')
-                blocks[i][j].appendChild(temp);
-                
-                var checkCount=0;
+var coverBlocks = function(n) {
+    for (var i=0; i<n; i++) {
+        for (var j=0; j<n; j++) {
+            var temp = document.createElement('div');
+            temp.classList.add('blockLayer')
+            blocks[i][j].appendChild(temp);
+            
+            var checkCount = 0;
 
-                temp.addEventListener('contextmenu', function(e) {
-                    e.preventDefault();
-                    if (!(toArray(e.target.classList).includes('flag'))) {
-                        e.target.classList.add('flag');
-                    }
-                    else {
-                        e.target.classList.remove('flag');
-                    }
-                    //document.querySelector('.open').play();
-                    
-                    if (checkCount==0) {
-                        counting();
-                        checkCount++;
-                    }
-                })
-                
-                temp.addEventListener('click', function(e) {
-                    if (toArray(e.target.parentNode.classList).includes('bomb') && !toArray(e.target.classList).includes('flag')) {
-                        e.target.style.display='none';
-                        setTimeout(gameOver, 200);
-                        console.log('GAME_OVER');
-                    }
-                    else if (!toArray(e.target.classList).includes('flag')) {
-                        e.target.classList.remove('blockLayer');
-                    }
-                    //document.querySelector('.flagAudio').play();
-                    
-                    if (checkCount==0) {
-                        counting();
-                        checkCount++;
-                    }
-                })
-    
-            }
+            temp.addEventListener('contextmenu', function(e) {
+                e.preventDefault();
+                if (!(toArray(e.target.classList).includes('flag'))) {
+                    e.target.classList.add('flag');
+                }
+                else {
+                    e.target.classList.remove('flag');
+                }
+                //document.querySelector('.open').play();
+                if (checkCount==0) {
+                    counting();
+                    checkCount++;
+                }
+            })
+            
+            temp.addEventListener('click', function(e) {
+                if (toArray(e.target.parentNode.classList).includes('bomb') && !toArray(e.target.classList).includes('flag')) {
+                    e.target.style.display = 'none';
+                    setTimeout(gameOver, 400);
+                    console.log('GAME_OVER');
+                }
+                else if (!toArray(e.target.classList).includes('flag')) {
+                    e.target.classList.remove('blockLayer');
+                }
+                //document.querySelector('.flagAudio').play();
+                if (checkCount == 0) {
+                    counting();
+                    checkCount++;
+                }
+            })
         }
+    }
 }
 
-var gameOver=function() {
-    var blocks=document.querySelectorAll('.block');
+var gameOver = function() {
+    var blocks = document.querySelectorAll('.block');
     toArray(blocks).forEach(function(elem) {
-        elem.style.display='none';
+        elem.style.display = 'none';
     });
-    document.querySelector('.gameOver').style.display='flex';
+    document.querySelector('.gameOver').style.display = 'flex';
     document.querySelector('.gOver').play();
-    document.querySelector('.rest').style.display='none';
+    document.querySelector('.rest').style.display = 'none';
     clearTimeout(timeout);
 }
-var restart=function() {
+
+var restart = function() {
     location.reload();
 }
+
 createMaze(8);
